a823d5af95c02ef28f2396e46634506552d71b00
Chris Laidler <cjjlaidler@gmail.com>
2021-02-24 11:50:22 +0000

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [character, setCharacter] = useState<any>();
+  const characterStats = ["Status", "Species", "Gender"];
+
+  const onGetCharacter = async (characterNumber: number) => {
+    try {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character/${characterNumber}`
+      );
+      const fetchedCharacter = await response.json();
+      setCharacter(fetchedCharacter);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getRandomNumber = () => {
+    const randomNumber = Math.ceil(Math.random() * 100);
+    return randomNumber;
+  };
+
+  const getCharacter = async () => {
+    const randomNumber = getRandomNumber();
+    await onGetCharacter(randomNumber);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {character && (
+        <div className="characterBox">
+          <h1>{character.name}</h1>
+          <img src={character.image} alt="Rick and Morty character" />
+          {characterStats.map((stat: string, idx: number) => (
+            <h4 key={`${stat}-${idx}`}>
+              {`${stat}: `}
+              <span className="statValue">{`${
+                character[stat.toLowerCase()]
+              }`}</span>
+            </h4>
+          ))}
+        </div>
+      )}
+      {!character && (
+        <div className="placeholderText">
+          Please click the button below to select a character
+        </div>
+      )}
+      <button onClick={() => getCharacter()}>Get character</button>
     </div>
   );
 }
